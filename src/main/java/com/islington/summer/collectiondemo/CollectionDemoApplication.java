505401a6d388c0314a7222ca0019a718177cd871
Aaju Maharjan <aaju.maharjan@islingtoncollege.edu.np>
2024-07-02 20:05:09 +0545

    add initial files

---
+++ src/main/java/com/islington/summer/collectiondemo/CollectionDemoApplication.java
@@ -0,0 +1,13 @@
+package com.islington.summer.collectiondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/islington/summer/collectiondemo/CollectionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.islington.summer.collectiondemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
